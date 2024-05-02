558e5207e27a751846b46c4275eede2fd106b7b9
Abhay <abhay@keyvalue.systems>
2024-05-02 19:15:57 +0530

    fix: a\updated app tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import Root from "./src/navigation/root";
+import { getStore } from "./src/store";
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Root />
+      <Provider store={getStore().store}>
+        <PersistGate loading={null} persistor={getStore().persistor}>
+          <Root />
+        </PersistGate>
+      </Provider>
     </SafeAreaView>
   );
 }
