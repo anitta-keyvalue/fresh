49e49ded61a673e222762c527cc214db861d85aa
Anitta Babu <anitta@keyvalue.systems>
2024-05-10 12:01:02 +0530

    feat: add logs in api

--- src/api/api.ts
+++ src/api/api.ts
@@ -14,7 +14,7 @@ import {clearStore} from '../store';
 const mutex = new Mutex();
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: Config.BASE_URL,
+  baseUrl: 'https://ybfqyi6i6g.execute-api.ap-southeast-1.amazonaws.com/default/rj-demo/',
   prepareHeaders: async headers => {
     headers.set('Content-Type', 'application/json');
     return headers;
@@ -27,7 +27,10 @@ const baseQuery = fetchBaseQuery({
 const customBaseQuery: BaseQueryFn = async (args, api, extraOptions) => {
   try {
     await mutex.waitForUnlock();
+    // console.log('args', args);
+    // console.log('api', api);
     let result: any = await baseQuery(args, api, extraOptions);
+    // console.log('result', result);
     return result;
   } catch (error) {
     return error;
@@ -38,7 +41,7 @@ const baseApi = createApi({
   baseQuery: customBaseQuery,
   endpoints: () => ({}),
   refetchOnMountOrArgChange: true,
-  tagTypes: [],
+  tagTypes: ['task'],
 });
 
 export default baseApi;
